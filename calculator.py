14b0386440736f079b141c43cd6b3243aafe1175
RafalTaraska <paulo.dorosz@gmail.com>
2022-11-03 23:02:21 +0100

    function modyfication - wersja dla chetnych

--- calculator.py
+++ calculator.py
@@ -1,5 +1,4 @@
 import logging
-import sys
 logging.basicConfig(level=logging.DEBUG)
 
 print("Podaj działanie, posługując się odpowiednią liczbą:")
@@ -11,59 +10,61 @@ operation = int(input("Jakie chcesz działanie 1/2/3/4: "))
 
 
 lista = []
-liczba = int(input("Podaj 1st liczbe: "))
+liczba = float(input("Podaj 1st liczbe: "))
 lista.append(liczba)
-liczba = int(input("Podaj 2st liczbe: "))
-
-
-
-     
+liczba = float(input("Podaj 2st liczbe: "))
+  
 
 # Deklaracja funkcji
 def add(lista):
-    add = sum(lista)
-    logging.info(f"Dodaję liczby {lista}")
-    print(f"Wynik to {add}")
-
+    suma = sum(lista)
+    return suma
+    
 def minus(lista):
-    minus = lista[0] - lista[1]
-    logging.info(f"Odejmuje liczby {lista}")
-    print(f"Wynik to {minus}")
+    subtraction = lista[0] - lista[1]
+    return subtraction
 
 def multiplication(lista):
     wynik = 1
     for liczba in lista:
         wynik = liczba * wynik
-    logging.info(f"Mnożę liczby {lista}")
-    print(f"Wynik to {wynik}")
+    return wynik
 
 def division(lista):
     result = lista[0] / lista[1]
-    logging.info(f"Dzielenie liczby {lista[0]} przez {lista[1]}")
-    print(f"Wynik to {result}")
+    return result
 
 
 # Wybór działania
 if operation == 1:
     while True: 
         lista.append(liczba)
-        liczba = int(input("Podaj kolejna liczbe, albo wciśnij 0 (zero) aby wykonac działanie: "))
+        liczba = float(input("Podaj kolejna liczbe, albo wciśnij 0 (zero) aby wykonac działanie: "))
         if liczba == 0:
             break
-    add(lista)
+    logging.info(f"Dodaję liczby {lista}")
+    suma = add(lista)
+    print(f"Wynik to {suma}")
+
 
 if operation == 2:
     lista.append(liczba)
-    minus(lista)
+    logging.info(f"Odejmuje od liczby {lista[0]} liczbę {lista[1]}" )
+    subtraction = minus(lista)
+    print(f"Wynik to {subtraction}")
 
 if operation == 3:
     while True: 
         lista.append(liczba)
-        liczba = int(input("Podaj kolejna liczbe, albo wciśnij 0 (zero) aby wykonac działanie: "))
+        liczba = float(input("Podaj kolejna liczbe, albo wciśnij 0 (zero) aby wykonac działanie: "))
         if liczba == 0:
             break
-    multiplication(lista)
+    logging.info(f"Mnożę liczby {lista}")
+    wynik = multiplication(lista)
+    print(f"Wynik to {wynik}")
 
 if operation == 4:
-    lista.append(liczba)
-    division(lista)
+    lista.append(liczba)   
+    logging.info(f"Dzielenie liczby {lista[0]} przez {lista[1]}")
+    result = division(lista)
+    print(f"Wynik to {result}")
